4d487dce9484ec0ff08581c3868e0dc32b2bc6a0
ConnorWolfe <Cwolfe25@gcds.net>
2022-12-05 17:58:56 -0500

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,88 +1,117 @@
-'''
-Created on Nov 8, 2022
+"""
+Created on Dec 5, 2022
 
 @author: CWolfe25
-'''
-
+"""
 def main():
-    board = [['1','2','3'],['4','5','6'],['7','8','9']]
+    board = [["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"]]
     win_condition = 0
     turn = 0
     print("Test")
-    count = 0 
-    while count == 0:
-        if board [0][0] == "x" and board [1][0] == "x" and board [2][0] == "x":
-            win_condition = 1
-        elif board [0][0] == "x" and board [0][1] == "x" and board [0][2] == "x":
-            win_condition = 1
-        elif board [0][0] == "x" and board [1][1] == "x" and board [2][2] == "x":
-            win_condition = 1
-        elif board [0][2] == "x" and board [1][1] == "x" and board [2][0] == "x":
-            win_condition = 1
-        elif board [1][0] == "x" and board [1][1] == "x" and board [1][2] == "x":
-            win_condition = 1
-        elif board [2][0] == "x" and board [2][1] == "x" and board [2][2] == "x":
-            win_condition = 1
-        elif board [0][1] == "x" and board [1][1] == "x" and board [2][1] == "x":
-            win_condition = 1
-        elif board [0][2] == "x" and board [1][2] == "x" and board [2][2] == "x":
-            win_condition = 1
-        elif board [0][0] == "O" and board [1][0] == "O" and board [2][0] == "O":
-            win_condition = 2
-        elif board [0][0] == "O" and board [0][1] == "O" and board [0][2] == "O":
-            win_condition = 2
-        elif board [0][0] == "O" and board [1][1] == "O" and board [2][2] == "O":
-            win_condition = 2
-        elif board [0][2] == "O" and board [1][1] == "O" and board [2][0] == "O":
-            win_condition = 2
-        elif board [1][0] == "O" and board [1][1] == "O" and board [1][2] == "O":
-            win_condition = 2
-        elif board [2][0] == "O" and board [2][1] == "O" and board [2][2] == "O":
-            win_condition = 2
-        elif board [0][1] == "O" and board [1][1] == "O" and board [2][1] == "O":
-            win_condition = 2
-        elif board [0][2] == "O" and board [1][2] == "O" and board [2][2] == "O":
-            win_condition = 2
-        else:
-            win_condition = 0
-        count = count+1
-    if win_condition == 0:
+    while win_condition == 0:
         if turn == 0 or turn == 2 or turn == 4 or turn == 6 or turn == 8:
-            print("enter in 0,1,2")
-            print("for row 0 = top 1 = middle 2 = bottom")
-            print("for column 0 = left 1 = middle 2 = right")
-            row_x = input("what row do you want to put X")
-            column_x = input("which column do you want to put X")
-            row_x = int(row_x)
-            column_x = int(column_x)
-            if row_x == 0 or row_x == 1 or row_x == 2 and column_x == 0 or column_x == 1 or column_x == 2:
-                print(board[row_x][column_x])
-                board[row_x][column_x] = "x"
+            print("x turn")
+            choice = input("what row do you want to put X 1-9")
+            choice = int(choice)
+            if choice == 1:
+                board[0][0] = "x"
+            elif choice == 2:
+                board[0][1] = "x"
+            elif choice == 3:
+                board[0][2] = "x"
+            elif choice == 4:
+                board[1][0] = "x" 
+            elif choice == 5:
+                board[1][1] = "x"
+            elif choice == 6:
+                board[1][2] = "x"
+            elif choice == 7:
+                board[2][0] = "x"
+            elif choice == 8:
+                board[2][1] = "x"
+            elif choice == 9:
+                board[2][2] = "x"
+            else:
+                print("enter in a number from 1-9")
         elif turn == 1 or turn == 3 or turn == 5 or turn == 7 or turn == 9:
-            print("enter in 0,1,2")
-            print("for row 0 = top 1 = middle 2 = bottom")
-            print("for column 0 = left 1 = middle 2 = right")
-            row_O = input("what row do you want to put O")
-            column_O = input("which column do you want to put O")
-            row_O = int(row_O)
-            column_O = int(column_O)
-            if row_O == 0 or row_O == 1 or row_O == 2 and column_O == 0 or column_O == 1 or column_O == 2:
-                board[row_O][column_O] = "O"
+            print("O turn")
+            choice = input("what row do you want to put X 1-9")
+            choice = int(choice)
+            if choice == 1:
+                board[0][0] = "O"
+            elif choice == 2:
+                board[0][1] = "O"
+            elif choice == 3:
+                board[0][2] = "O"
+            elif choice == 4:
+                board[1][0] = "O" 
+            elif choice == 5:
+                board[1][1] = "O"
+            elif choice == 6:
+                board[1][2] = "O"
+            elif choice == 7:
+                board[2][0] = "O"
+            elif choice == 8:
+                board[2][1] = "O"
+            elif choice == 9:
+                board[2][2] = "O"
+            else:
+                print("enter in a number from 1-9")
         turn = turn + 1
         print(board)
-    elif win_condition == 1:
-        print("x won")
-    elif win_condition == 2:
-        print("O won")
-    elif win_condition == 0 and turn >10:
-        print("its a tie")
+        win_condition = win(board,win_condition,turn)
+        if win_condition == 1:
+            print("x won")
+        elif win_condition == 2:
+            print("O won")
+        elif win_condition == 3:
+            print("its a tie")
+        else:
+            print("something went wrong")
+
+
+
+def win(board,win_condition,turn):
+    if board[0][0] == "x" and board[1][0] == "x" and board[2][0] == "x":
+        win_condition = 1
+    elif board[0][0] == "x" and board[0][1] == "x" and board[0][2] == "x":
+        win_condition = 1
+    elif board[0][0] == "x" and board[1][1] == "x" and board[2][2] == "x":
+        win_condition = 1
+    elif board[0][2] == "x" and board[1][1] == "x" and board[2][0] == "x":
+        win_condition = 1
+    elif board[1][0] == "x" and board[1][1] == "x" and board[1][2] == "x":
+        win_condition = 1
+    elif board[2][0] == "x" and board[2][1] == "x" and board[2][2] == "x":
+        win_condition = 1
+    elif board[0][1] == "x" and board[1][1] == "x" and board[2][1] == "x":
+        win_condition = 1
+    elif board[0][2] == "x" and board[1][2] == "x" and board[2][2] == "x":
+        win_condition = 1
+    elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+        win_condition = 2
+    elif board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+        win_condition = 2
+    elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+        win_condition = 2
+    elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+        win_condition = 2
+    elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+        win_condition = 2
+    elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+        win_condition = 2
+    elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+        win_condition = 2
+    elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+        win_condition = 2
+    elif turn <= 0: 
+        win_condition = 3 
     else:
-        print("something went wrong")
+        win_condition = 0
+    return win_condition
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
