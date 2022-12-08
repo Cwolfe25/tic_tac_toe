064286175e37bfd0c8426cdb81d0c3a3513c4ae1
ConnorWolfe <Cwolfe25@gcds.net>
2022-12-08 15:03:12 -0500

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -25,53 +25,37 @@ def main():
     print("Test")
     while win_condition == 0:                                                   #this will end the game when a winner is found
         if turn == 0 or turn == 2 or turn == 4 or turn == 6 or turn == 8:       #this shows the turn that alternates into x
-            print("x turn")
-            choice = input("what row do you want to put X 1-9")                 #askes the user wich square they put it into
-            choice = int(choice)
-            if choice == 1:                                                     #following if/elif statements find which one they wanted to choose
-                board[0][0] = "x"
-            elif choice == 2:
-                board[0][1] = "x"
-            elif choice == 3:
-                board[0][2] = "x"
-            elif choice == 4:
-                board[1][0] = "x" 
-            elif choice == 5:
-                board[1][1] = "x"
-            elif choice == 6:
-                board[1][2] = "x"
-            elif choice == 7:
-                board[2][0] = "x"
-            elif choice == 8:
-                board[2][1] = "x"
-            elif choice == 9:
-                board[2][2] = "x"
+            print("O turn")
+            x_row = input("what row do you want")
+            x_col = input("what column do oyu want")
+            x_row = int(x_row)
+            x_col = int(x_col)
+            x_row == x_row - 1
+            x_col == x_col - 1
+            if x_row > 2 or x_row <0:
+                print("enter in a valid row")
+            elif x_col > 2 or x_col <0:
+                print("enter in a valid column")
+            elif board[x_row][x_col] == "x" or board[x_row][x_col] == "O":
+                print("this has already been chosen")   
             else:
-                print("enter in a number from 1-9")
+                board[x_row][x_col] == "O"
         elif turn == 1 or turn == 3 or turn == 5 or turn == 7 or turn == 9:        #This see's if its Os turn
             print("O turn")
-            choice = input("what row do you want to put X 1-9")
-            choice = int(choice)
-            if choice == 1:
-                board[0][0] = "O"                                                   #finds all of the places they could have put it 
-            elif choice == 2:
-                board[0][1] = "O"
-            elif choice == 3:
-                board[0][2] = "O"
-            elif choice == 4:
-                board[1][0] = "O" 
-            elif choice == 5:
-                board[1][1] = "O"
-            elif choice == 6:
-                board[1][2] = "O"
-            elif choice == 7:
-                board[2][0] = "O"
-            elif choice == 8:
-                board[2][1] = "O"
-            elif choice == 9:
-                board[2][2] = "O"
+            O_row = input("what row do you want")
+            O_col = input("what column do oyu want")
+            O_row = int(O_row)
+            O_col = int(O_col)
+            O_row == O_row - 1
+            O_col == O_col - 1
+            if O_row > 2 or O_row <0:
+                print("enter in a valid row")
+            elif O_col > 2 or O_col <0:
+                print("enter in a valid column")
+            elif board[O_row][O_col] == "x" or board[O_row][O_col] == "O":
+                print("this has already been chosen")   
             else:
-                print("enter in a number from 1-9")
+                board[O_row][O_col] == "O"
         turn = turn + 1
         print(board)
         win_condition = win(board,win_condition,turn)                           #checks to see if ther is a winner
