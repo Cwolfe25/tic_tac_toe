c440d174f5822980f2636d5e7b3812434d292ee5
ConnorWolfe <Cwolfe25@gcds.net>
2022-12-14 08:35:03 -0500

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -21,7 +21,7 @@ def comp_choice(comp_board):
         comp_board[comp_row][comp_col] = "v"
         times == times - 1
     return(comp_board)
-def user_choice(player_show,comp_board):
+def user_choice(player_show,comp_board,user_check):
     print(player_show)
     print("enter 1 - 5 ")
     user_row = input("what row do you chose?")
@@ -30,14 +30,32 @@ def user_choice(player_show,comp_board):
     user_col == user_col - 1 
     if comp_board[user_row][user_col] == "v":
         player_show[user_row][user_col] = "x"
+        user_check == user_check + 1
     else:
         player_show[user_row][user_col] = "o"
     print(player_show)
-    return(player_show,comp_show)
-def comp_v_user(player_board):
+    return(player_show,user_check)
+def comp_v_user(player_board,comp_show,comp_check):
     row_comp = random.randint(0,4)
     col_comp = random.randint(0,4)
     if player_board[row_comp][col_comp] == "v":
+        comp_show[row_comp][col_comp] == "x"
+        comp_check == comp_check + 1
+    else:
+        comp_show[row_comp][col_comp]
+    return(comp_show,comp_check)
+def winner(comp_show,player_show,win_condition,player_check,comp_check):
+    print("ok")
+    comp_counter = 0 
+    play_counter = 0 
+    if player_check == 4:
+        win_condition = 1
+    if comp_check == 4:
+        win_condition = 2
+    else:
+        win_coundition = 0 
+    return(win_condition)
+
 
 
 def main():
@@ -49,10 +67,25 @@ def main():
     comp_board = comp_choice(comp_board)
     turn = 0 
     win_condition = 0
+    user_check = 0 
+    comp_check = 0
     while win_condition == 0:
         if turn == 0 or turn == 2 or turn == 4 or turn == 6 or turn == 8 or turn == 10:
-            player_choice = user_choice(player_show,comp_board)
+            temp_show = user_choice(player_show,comp_board,user_check)
+            temp_show = temp_show.split(",")
+            player_show = temp_show[0]
+            user_check = temp_show[1]
+        elif turn == 1 or turn == 3 or turn == 5 or turn == 7 or turn == 9 or turn ==11:
+            temp_show = comp_v_user(player_board,comp_show,comp_check)
+            temp_show = temp_show.split(",")
+            comp_show = temp_show[0]
+            comp_check = temp_show[1]
+        win_condition = winner(comp_show,player_show,win_condition)
         turn == turn + 1
+    if win_condition == 1:
+        print("computer won")
+    elif win_condition == 2:
+        print("you won")
 
 
 
