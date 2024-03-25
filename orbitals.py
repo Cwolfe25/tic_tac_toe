0a3a9af9896861e3c738c8a10c13235af2497d2c
ConnorWolfe <Cwolfe25@gcds.net>
2024-03-24 20:08:34 -0400

    Update orbitals.py

--- orbitals.py
+++ orbitals.py
@@ -7,79 +7,66 @@ from _csv import writer
 # Define the figure and axis
 fig, ax = plt.subplots()
 # Define the ellipse parameters
-"""
-a = 400000
-b = 200000
-
-c = a**2 - b**2
-c = abs(c)
-c = math.sqrt(c)
-print(c)
-"""
 x0 = 2000000                        #distnace from the earth
 y0 = 0
+
 G = 6.67*10**-11                    #gravity constant
-print(G)
 mH = 5.97*10**24                    #mass of the planet
-print(mH)
-#formulas
-#div = x**2 + y**2
-#div = div **1.5
-#aax = -1 * mH*G*a / div
-#aay = mH*G*b / div
-#vx = vx + ax * i
-#vy = vy + ax * i 
-#x = x + vx * i
-#y = y + vy * i 
+
 vx0 = 0 
 vy0 = 15000                          #intial velocities
+
 div = x0**2 + y0**2                 #gets the bottom of the acceleration equation
 div = div **1.5
-print(div)
+
 aax0 = -1 * G*mH *x0 / div          #acceleration of the x
-print("x accel")
-print(aax0)
 aay0 = -1 * G *mH*y0/div            #accleration of the y
-print(aay0)
+
 # Define the animation function
 inter = 1
-it = 0
-data = [vx0,vy0,x0,y0,aax0,aay0,inter,it]              #takes the initial conditions
+it = 0                              #this was included for the kepler proof
+data = [vx0,vy0,x0,y0,aax0,aay0,inter,it]              #takes the initial conditions in a list
 
 def animate(i):
 # Clear the axis for each iteration
     ax.clear()
     inter= data[6]
-    print(inter)
     aax = data[4]
     aay = data[5]
     vx = data[0]
     vy = data[1]
+
     vx = vx + aax * inter                       #uses the acceleration of the x times the interval to get a new acceration
     vy = vy - aay * inter                       #NEEDS TO BE MINUES BC ITS WEIRD
-    print("velocity of x")
-    print(vx)
-    print("velocity of y")
-    print(vy)
-    data[0] = vx                                #sets them into the data
+
+    data[0] = vx                                #redefines data
     data[1] = vy
+
     x = data[2]
     yzor = data[3]
-    x = x + vx * inter                          #uses the velocity to find the x and y 
+
+    x = x + vx * inter                          #uses the velocity to find the x and y positions
     yzor = yzor + vy * inter 
+
     data[2] = x
     data[3] = yzor
+
     div = x**2 + yzor**2
     div = div **1.5
-    aax = -1 * mH*G*x / div                     #finds the new acceleration
+    aax = -1 * mH*G*x / div                     #finds the new accelerations
     aay = 1 * mH*G*yzor / div
+
     data[4] = aax
     data[5] = aay
+
     # Plot the ellipse
     ax.plot(x, yzor, 'ro')                      #plots the cordinates
     t = data[7]
     t = t +inter
     data[7] = t
+    print(yzor)
+    print(t)
+    #bellow is the code I used to create the planet csv file
     """
     with open("planet.csv","a", newline = "")as add:
         csv = [t,x,yzor]
